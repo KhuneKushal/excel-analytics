6e9f2c4d4464ceaa1d61adee1845cb9a380419dc
KhuneKushal <kushalkhune111@gmail.com>
2025-08-15 06:41:15 -0700

    fixing vercel errors

--- dist/excel-analytics/server/angular-app-manifest.mjs
+++ dist/excel-analytics/server/angular-app-manifest.mjs
@@ -41,9 +41,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 70072, hash: '23af6b91172e0ea6d572a097f450d512803c6fe0a7a8fd2029c95be8dd0fdfa3', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 17170, hash: 'c51fe0e0a879c071187838943c016885d47c9e20bce51f8b1b436eb5b8b9b44c', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'data-summary/index.html': {size: 171056, hash: 'b05142e0582a99838f08e790ec4e656ee3aeaee63968b698ebcf522ae5b947f1', text: () => import('./assets-chunks/data-summary_index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 70072, hash: '5ca80285d22a927ec9533af515d780b149dd4cbc8ce5b5ac45d34f1bb88c6a15', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 17170, hash: '95e90afcc92a3b5f717714721c33b664674276d7135053fa50325c11f4b0fad5', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'data-summary/index.html': {size: 171056, hash: '12220cf2ad2e0c758b8dfd3c146cf9e5e6168479c67666231611ed9aea0414b3', text: () => import('./assets-chunks/data-summary_index_html.mjs').then(m => m.default)},
     'styles-Q4YAEP73.css': {size: 104387, hash: '9kn01rNuYoQ', text: () => import('./assets-chunks/styles-Q4YAEP73_css.mjs').then(m => m.default)}
   },
 };
