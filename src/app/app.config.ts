eff0c6cd4e65cfeaeb165f5b0a4310df4267ecb8
KhuneKushal <kushalkhune111@gmail.com>
2025-09-15 14:32:00 -0700

    last one of the day

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,12 @@
 import { ApplicationConfig, provideZoneChangeDetection, ErrorHandler } from '@angular/core';
 import { provideRouter, withNavigationErrorHandler } from '@angular/router';
 import { provideAnimations as provideClientAnimations } from '@angular/platform-browser/animations';
-import { routes } from './app.routes';
+
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import 'chartjs-adapter-date-fns';
 import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
+import { routes } from './app.routes';
 import { GlobalErrorHandler } from './services/global-error.handler';
 
 export const appConfig: ApplicationConfig = {
